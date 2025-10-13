155fda3f0a0898380b968cab692abdfedc14248f
Shahab Faderani <s.faderani@avm.de>
2025-10-13 14:25:11 +0200

    FEATURE: connect frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,7 +8,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+  res.send({ message: "hello World!" });
+});
+
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello, World!" });
 });
 
 app.listen(port, () => {
