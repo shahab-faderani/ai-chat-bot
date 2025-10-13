b077e67258eb936174a245045ccf1f0818b4afea
Shahab Faderani <s.faderani@avm.de>
2025-10-13 15:08:33 +0200

    FEATURE: set up prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
-import { Button } from "./components/ui/button";
+import { useState, useEffect } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch("/api/hello")
+    fetch('/api/hello')
       .then((response) => response.json())
       .then((data) => setMessage(data.message));
   }, []);
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send({ message: "hello World!" });
+app.get('/', (req: Request, res: Response) => {
+  res.send({ message: 'hello World!' });
 });
 
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({ message: "Hello, World!" });
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'Hello, World!' });
 });
 
 app.listen(port, () => {
